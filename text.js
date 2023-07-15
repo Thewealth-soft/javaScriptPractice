fb5d812270c4579ffe5eb90bb970954848e82647
Thewealth <odewumiolajide@gmail.com>
2023-07-15 13:07:35 -0700

    The 4th trial

---
+++ text.js
@@ -0,0 +1,14 @@
+
+let prompt = require("prompt-sync")({ sigint: true });
+
+
+
+//using the loop and list write the prog that console the sum of 10 number in the list
+
+let numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let totalNum = 0
+
+for (let i = 0; i < numList.length; i++){
+  // totalNum += numList[i]
+  console.log(numList[i]) 
+}
